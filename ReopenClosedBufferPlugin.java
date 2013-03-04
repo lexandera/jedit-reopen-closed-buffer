be4af3973ab95a55606738301ca66612783ec0e1
Aleksander Kmetec <aleksander.kmetec@gmail.com>
2013-03-04 02:44:47 +0100

    Comment tweaks

--- ReopenClosedBufferPlugin.java
+++ ReopenClosedBufferPlugin.java
@@ -1,7 +1,7 @@
 /*
  * ReopenClosedBufferPlugin.java
  * part of the ReopenClosedBuffer plugin for the jEdit text editor
- * Copyright (C) 2001 Aleksander Kmetec
+ * Copyright (C) 2013 Aleksander Kmetec
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -16,8 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * $Id$
  */
 
 import org.gjt.sp.jedit.*;
